4bc8468693d2dd58771d19d984d545eeee5eab39
edwinvb <evb1915@gmail.com>
2019-11-06 20:36:15 -0600

    push

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,2 +1,21 @@
 export class Satellite {
+	name: string;
+	orbitType: string;
+	type: string;
+	launchDate: string;
+	operational: boolean;
+	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+		this.name = name;
+		this.orbitType = orbitType;
+		this.type = type;
+		this.operational = operational;
+		this.launchDate = launchDate;
+	}
+	shouldShowWarning() {
+		if (this.type === 'Space Debris') {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
